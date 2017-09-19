0a4daf369e12a3e713746bfc65c4631ffa5fc3b1
Akoni <hakmorrison@csumb.edu>
2017-09-19 21:50:47 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta>
+        <title>Homework 2</title>
+    </head>
+    <body>
+        
+        <?php
+        
+        echo "Hello";
+        
+        
+        
+        
+        ?>
+        
+        
+    </body>
+</html>
