2e1c928e3162cf9dae0fd9767c69f3c37b86f042
Akoni <hakmorrison@csumb.edu>
2017-09-19 23:45:21 +0000

    proposed dbz generator

--- index.php
+++ index.php
@@ -2,13 +2,24 @@
 <html>
     <head>
         <meta>
-        <title>Homework 2</title>
+        <title>DBZ Generator</title>
     </head>
     <body>
         
         <?php
         
-        echo "Hello";
+        echo "<h1>Dragon Ball Z Generator </h1>";
+        
+        /*
+        Must include at least two loops (e.g., for, while, do while)
+        Must include at least two conditions  (e.g., if, switch, ternary)
+        Must have at least 15 CSS rules in an external file
+        Must use the rand() function at least once
+        Uses at least two images
+        Uses at least one array
+        Uses at least three array functions
+        All elements (loops, arrays, images, etc.) have a common goal 
+        */
         
         
         
