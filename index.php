bbe26da52ccafed8ef1cb004b287d1e86ff341bb
Akoni <hakmorrison@csumb.edu>
2017-09-20 22:17:52 +0000

    added images

--- index.php
+++ index.php
@@ -3,27 +3,66 @@
     <head>
         <meta>
         <title>DBZ Generator</title>
+        <link rel="stylesheet" href="styles.css" type="text/css" />
     </head>
     <body>
         
         <?php
-        
-        echo "<h1>Dragon Ball Z Generator </h1>";
-        
-        /*
-        Must include at least two loops (e.g., for, while, do while)
-        Must include at least two conditions  (e.g., if, switch, ternary)
-        Must have at least 15 CSS rules in an external file
-        Must use the rand() function at least once
-        Uses at least two images
-        Uses at least one array
-        Uses at least three array functions
-        All elements (loops, arrays, images, etc.) have a common goal 
-        */
-        
-        
-        
-        
+            echo "<div id=container>";   
+                echo "<h1>Dragon Ball Z Generator </h1>";
+                
+                /*
+                Must include at least two loops (e.g., for, while, do while)
+                Must include at least two conditions  (e.g., if, switch, ternary)
+                Must have at least 15 CSS rules in an external file
+                Must use the rand() function at least once
+                Uses at least two images
+                Uses at least one array
+                Uses at least three array functions
+                All elements (loops, arrays, images, etc.) have a common goal 
+                */
+                
+                $sayain = array("Goku","Vegeta","Gohan","Goten","Trunks","Napa","Raditz","Bardock", "Broly");
+                $namekian = array("Piccolo", "Dende","Kami","Nail","Guru");
+                $frieza = array("Frieza","Cooler","King_Cold","Zarbon","Dodoria");
+                $human = array("Hercule","Videl","Yamcha", "Krillin", "Roshi","Bulma","ChiChi");
+                
+                $sum = 0;
+                
+                
+                for($i = 0; $i < count($sayain); $i++) {
+                    $sum++;
+                }
+                echo "Amount of Saiyans: ".$sum."<br>";
+                $sum = 0;
+                
+                for($i = 0; $i < count($namekian); $i++) {
+                    $sum++;
+                }
+                echo "Amount of Namekians: ".$sum."<br>";
+                $sum=0;
+                
+                for($i = 0; $i < count($frieza); $i++) {
+                    $sum++;
+                }
+                echo "Amount of Frieza Army: ".$sum."<br>";
+                $sum = 0;
+                
+                for($i = 0; $i < count($human); $i++) {
+                    $sum++;
+                }
+                echo "Amount of Humans: ".$sum."<br>";
+                $sum = 0;
+                
+                
+                echo "Random sayain: ".array_rand($sayain) ."<br>";
+                echo "Random namekian: ".array_rand($namekian)."<br>";
+                echo "Random frieza: ".array_rand($frieza)."<br>";
+                echo "Random human: ".array_rand($human)."<br>";
+                
+                
+            echo "</div>";     
+                
         ?>
         
         
