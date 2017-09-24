51e76aa1a762dea51519d06c73906da4f35cf51e
Akoni <hakmorrison@csumb.edu>
2017-09-24 21:26:48 +0000

    added image/updated structure

--- index.php
+++ index.php
@@ -22,87 +22,62 @@
                 All elements (loops, arrays, images, etc.) have a common goal 
                 */
                 
-                $sayain = array("Goku","Vegeta","Gohan","Goten","Trunks","Napa","Raditz","Bardock", "Broly");
-                $namekian = array("Piccolo", "Dende","Kami","Nail","Guru");
-                $frieza = array("Frieza","Cooler","King_Cold","Zarbon","Dodoria");
-                $human = array("Hercule","Videl","Yamcha", "Krillin", "Roshi","Bulma","ChiChi");
+
+                $randomValue = rand(0,3);
                 
-                $sum = 0;
+                generateCharacter($randomValue);
                 
-                
-                for($i = 0; $i < count($sayain); $i++) {
-                    $sum++;
-                }
-                echo "Amount of possible Saiyans: ".$sum."<br>";
-                $sum = 0;
-                
-                for($i = 0; $i < count($namekian); $i++) {
-                    $sum++;
-                }
-                echo "Amount of possible Namekians: ".$sum."<br>";
-                $sum=0;
-                
-                for($i = 0; $i < count($frieza); $i++) {
-                    $sum++;
-                }
-                echo "Amount of possible Frieza Army: ".$sum."<br>";
-                $sum = 0;
-                
-                for($i = 0; $i < count($human); $i++) {
-                    $sum++;
-                }
-                echo "Amount of possible Humans: ".$sum."<br>";
-                $sum = 0;
-                
-                // echo "Random sayain: ".array_rand($sayain) ."<br>";
-                // echo "Random namekian: ".array_rand($namekian)."<br>";
-                // echo "Random frieza: ".array_rand($frieza)."<br>";
-                // echo "Random human: ".array_rand($human)."<br>";
+            echo "</div>";  
+
                 
                 
-                function displayCharacter($randomValue) {
-                    
-                $saiyan = array("Goku","Vegeta","Gohan","Goten","Trunks","Napa","Raditz","Bardock", "Broly");
-                $namekian = array("Piccolo", "Dende","Kami","Nail","Guru");
-                $frieza = array("Frieza","Cooler","King_Cold","Zarbon","Dodoria");
-                $human = array("Hercule","Videl","Yamcha", "Krillin", "Roshi","Bulma","ChiChi");
-                    
+                function generateCharacter($randomValue) {
+
+                    $saiyan = array("Goku","Vegeta","Gohan","Goten","Trunks","Napa","Raditz","Bardock", "Broly");
+                    $namekian = array("Piccolo", "Dende","Kami","Nail","Guru");
+                    $frieza = array("Frieza","Cooler","King_Cold","Zarbon","Dodoria");
+                    $human = array("Hercule","Videl","Yamcha", "Krillin", "Roshi","Bulma","ChiChi");
+                    $sum = 0;
                     
                     switch ($randomValue) {
                         case 0:
+                            array_push($saiyan, "King_Vegeta");
+                            for($i = 0; $i < count($saiyan); $i++) {
+                                 $sum++;
+                            }
+                            echo "Amount of possible Saiyans: ".$sum."<br>";
                             $character = array_rand($saiyan);
-                            //$character = $saiyan[mt_rand(0,count($saiyan) -1)];
                             $person = $saiyan[$character];
+                            array_pop($saiyan);
                             break;
                         case 1:
+                            for($i = 0; $i < count($namekian); $i++) {
+                                $sum++;
+                            }
+                            echo "Amount of possible Namekians: ".$sum."<br>";
                             $character = array_rand($namekian);
-                           // $character = $namekian[mt_rand(0,count($namekian) -1)];
                             $person = $namekian[$character];
                             break;
                         case 2:
+                            for($i = 0; $i < count($frieza); $i++) {
+                            $sum++;
+                            }
+                             echo "Amount of possible Frieza Army: ".$sum."<br>";
                             $character = array_rand($frieza);
-                            //$character = $frieza[mt_rand(0,count($frieza) -1)];
                             $person = $frieza[$character];
                             break;
                         case 3:
+                            for($i = 0; $i < count($human); $i++) {
+                            $sum++;
+                            }
+                            echo "Amount of possible Humans: ".$sum."<br>";
                             $character = array_rand($human);
                             $person = $human[$character];
-                            //$character = $human[mt_rand(0,count($human) -1)];
                             break;
                     }
                      echo "Your character is: ".$person."<br>"; 
                      echo "<img src='./images/$person.jpg' alt='$person' title='".ucfirst($person)."'width='300'/>";
-                     //echo "Your character is: ".$character."<br>";
-                     
                 }
-                
-                
-                $randomValue = rand(0,3);
-                
-                displayCharacter($randomValue);
-            
-                
-            echo "</div>";     
                 
         ?>
         
